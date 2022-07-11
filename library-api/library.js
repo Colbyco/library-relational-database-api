15d5c95a27219d9e476e0465683efdcee950e1a6
Colby Baker <Colbyco017@gmail.com>
2022-07-11 00:47:52 -0400

    fix SQL again

--- library-api/library.js
+++ library-api/library.js
@@ -3,7 +3,7 @@ const db = require('./db');
 async function searchByAuthor(input) {
     let response = await db.raw(`SELECT author_id FROM author WHERE author_name = :input`, { input });
     let authorId = response.rows[0].author_id;
-    response = await db.raw(`SELECT book_name FROM book WHERE author_id = :author_id`, { authorId });
+    response = await db.raw(`SELECT book_name FROM book WHERE author_id = :authorId`, { authorId });
     return response.rows;
 }
 
