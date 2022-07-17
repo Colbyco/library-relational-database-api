6de074d01eec8d2844025667e6a3cabb684cd4b3
Colby Baker <59655851+Colbyco@users.noreply.github.com>
2022-07-16 21:39:44 -0400

    mvp
    * commit
    
    * fix SQL
    
    * fix SQL again
    
    * mvp

--- library-api/library.js
+++ library-api/library.js
@@ -1,12 +1,62 @@
+const knex = require('./db');
 const db = require('./db');
 
-async function searchByAuthor(input) {
-    let response = await db.raw(`SELECT author_id FROM author WHERE author_name = ?`, [input] )
-    let author_id = response.rows[0].author_id;
-    response = await db.raw(`SELECT book_name FROM book WHERE author_id = ?`, [author_id])
-    return response.rows
+// Create
+async function createAuthor(author_name) {
+    return await db('author').insert({ author_name }).returning(['author_id', 'author_name']);
+}
+
+async function createBook({ book_name, author_id, published_date, owned, on_hand }) {
+    return await db('book')
+        .insert({ book_name, author_id, published_date, owned, on_hand })
+        .returning(['book_id', 'book_name', 'author_id', 'published_date', 'owned', 'on_hand']);
+}
+
+// Read
+async function getAuthors() {
+    return await db('author').select('*');
+}
+
+async function getBooks() {
+    return await db('book').select('*');
+}
+
+async function findAuthor(author_id) {
+    return await db('author').select('author_name').where({ author_id });
+}
+
+async function findBook(book_id) {
+    return await db('book').select('*').where({ book_id });
+}
+
+// Update
+async function updateAuthor(author_id, author_name) {
+    return await db('author').update({ author_name }).where({ author_id }).returning(['author_id', 'author_name']);
+}
+
+async function updateBook(book_id, { book_name, author_id, published_date, owned, on_hand }) {
+    return await db('book')
+        .update({ book_name, author_id, published_date, owned, on_hand })
+        .where({ book_id })
+        .returning(['book_id', 'book_name', 'author_id', 'published_date', 'owned', 'on_hand']);
+}
+
+// Delete
+async function deleteBook(book_id) {
+    return await db('book')
+        .where({ book_id })
+        .del()
+        .returning(['book_id', 'book_name', 'author_id', 'published_date', 'owned', 'on_hand']);
 }
 
 module.exports = {
-    searchByAuthor
-};  
+    createAuthor,
+    createBook,
+    getAuthors,
+    getBooks,
+    findAuthor,
+    findBook,
+    updateAuthor,
+    updateBook,
+    deleteBook,
+};
